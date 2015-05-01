ef9194f0c4b36b40e94145ea542a376f5c5e608f
Tiago Royer <royertiago@gmail.com>
2015-05-01 17:45:35 -0300

    Add test/test.cpp.

---
+++ test/test.cpp
@@ -0,0 +1,7 @@
+/* test.cpp
+ * File that contains the 'main' function for the tests.
+ *
+ * The actual test cases are in other .test.cpp files.
+ */
+#define CATCH_CONFIG_MAIN
+#include <catch.hpp>
